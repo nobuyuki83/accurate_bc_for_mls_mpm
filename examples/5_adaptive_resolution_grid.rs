abc843628f2c6cc1e14ef3b764a948c35d9f49e0
Honoluluchampi <msd13aj@gmail.com>
2023-10-14 16:13:16 +0900

    output points

--- examples/5_adaptive_resolution_grid.rs
+++ examples/5_adaptive_resolution_grid.rs
@@ -3,6 +3,7 @@ use std::ops::Add;
 type Real = f32;
 type Vec2 = nalgebra::Vector2::<Real>;
 
+#[derive(PartialEq)]
 enum GridState {
     EMPTY,
     FILLED,
@@ -10,7 +11,7 @@ enum GridState {
 }
 
 fn cross(target : &Vec2, t0 : &Vec2, t1 : &Vec2) -> Real
-{ (t1.x - t0.x) * target.y - (t1.y - t0.y) * target.x }
+{ -(t1.x - t0.x) * target.y + (t1.y - t0.y) * target.x }
 
 fn is_inside_triangle(target : &Vec2, t0 : &Vec2, t1 : &Vec2, t2 : &Vec2) -> bool
 {
@@ -23,16 +24,16 @@ fn create_coarse_grid(poly : &Vec::<Vec2>, n : usize) -> Vec::<GridState>
 {
     let mut grid = Vec::<GridState>::new();
 
-    let dx = 1.0 / n;
+    let dx = 1.0 / n as Real;
 
-    for j in 0..n {
-        for i in 0..n {
+    for i in 0..n {
+        for j in 0..n {
             let points : [Vec2; 4];
             points = [
-                Vec2::new(i * dx, j * dx),
-                Vec2::new((i + 1) * dx, j * dx),
-                Vec2::new(i * dx, (j + 1) * dx),
-                Vec2::new((i + 1) * dx, (j + 1) * dx)
+                Vec2::new(i as Real * dx, j as Real * dx),
+                Vec2::new((i + 1) as Real * dx, j as Real * dx),
+                Vec2::new(i as Real * dx, (j + 1) as Real * dx),
+                Vec2::new((i + 1) as Real * dx, (j + 1) as Real * dx)
             ];
 
             // count the number of points inside the triangle
@@ -63,13 +64,13 @@ fn main() {
     const N: usize = 10;
     const M: usize = N + 1;
 
-    let poly0 = vec!(
+    let poly = vec!(
         Vec2::new(0.141, 0.151),
         Vec2::new(0.883, 0.151),
         Vec2::new(0.511, 0.681),
     );
 
-    let poly1 = intersections_polygon_gird(&poly0, N);
+    let coarse_grid = create_coarse_grid(&poly, N);
 
     use mpm2::canvas::Canvas;
     let mut canvas = Canvas::new((827, 827));
@@ -84,22 +85,32 @@ fn main() {
         canvas.paint_line(0., y, canvas.width as Real, y,
                           0.5, 0x00888888);
     }
-    for iline in 0..poly1.len() {
+    for iline in 0..poly.len() {
         let ip0 = iline;
-        let ip1 = (iline + 1) % poly1.len();
+        let ip1 = (iline + 1) % poly.len();
         canvas.paint_line(
-            poly1[ip0].x * canvas.width as Real,
-            poly1[ip0].y * canvas.height as Real,
-            poly1[ip1].x * canvas.width as Real,
-            poly1[ip1].y * canvas.height as Real,
+            poly[ip0].x * canvas.width as Real,
+            poly[ip0].y * canvas.height as Real,
+            poly[ip1].x * canvas.width as Real,
+            poly[ip1].y * canvas.height as Real,
             2., 0x00ffffff);
     }
-    for p in poly1.iter() {
-        canvas.paint_circle(
-            p.x * canvas.width as Real,
-            p.y * canvas.height as Real,
-            4., 0x00ff00ff);
+    for i in 0..N {
+        for j in 0..N {
+            if coarse_grid[i * N + j] == GridState::EMPTY {
+                canvas.paint_circle(
+                    (i as Real + 0.5) / N as Real * canvas.width as Real,
+                    (j as Real + 0.5) / N as Real * canvas.height as Real,
+                    4., 0x00ff00ff);
+            }
+            else if coarse_grid[i * N + j] == GridState::FILLED {
+                canvas.paint_circle(
+                    (i as Real + 0.5) / N as Real * canvas.width as Real,
+                    (j as Real + 0.5) / N as Real * canvas.height as Real,
+                    4., 0x00ffffff);
+            }
+        }
     }
 
-    canvas.write(&std::path::Path::new("2.png"));
+    canvas.write(&std::path::Path::new("5.png"));
 }
